03922b78dae99ac032fc4c2b2816bddb1413c371
Krishna Rathi <krishna.rathi.1209@gmail.com>
2025-10-25 23:27:53 +0530

    Changes

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, send_file
 import io
 from datetime import datetime
+import os
 from bill_template import generate_invoice
 
 app = Flask(__name__)
@@ -53,4 +54,5 @@ def download():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
